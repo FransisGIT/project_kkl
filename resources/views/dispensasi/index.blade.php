21da8e60a0afca4f7b9e45d1bb177cd5537b1b76
Diva Fran <diva.fran@example.com>
2025-12-10 20:01:56 +0700

    menambah css

--- resources/views/dispensasi/index.blade.php
+++ resources/views/dispensasi/index.blade.php
@@ -1,55 +1,57 @@
 @extends('app', ['title' => 'Riwayat Dispensasi'])
 
 @section('content')
+    <div class="card p-4 shadow-sm">
+        <h4>Riwayat Pengajuan Dispensasi</h4>
+        <hr>
 
-<div class="card p-4 shadow-sm">
-    <h4>Riwayat Pengajuan Dispensasi</h4>
-    <hr>
-
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Tahun Akademik</th>
-                <th>Jumlah</th>
-                <th>No HP</th>
-                <th>Deadline</th>
-                <th>Status</th>
-                <th>File</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            @forelse ($data as $d)
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $d->tahun_akademik }}</td>
-                    <td>{{ $d->jumlah_pengajuan }}</td>
-                    <td>{{ $d->no_hp }}</td>
-                    <td>{{ $d->tanggal_deadline }}</td>
-                    <td>
-                        @if ($d->status == 'menunggu')
-                            <span class="badge bg-warning">Menunggu</span>
-                        @elseif ($d->status == 'disetujui')
-                            <span class="badge bg-success">Disetujui</span>
-                        @else
-                            <span class="badge bg-danger">Ditolak</span>
-                        @endif
-                    </td>
-                    <td>
-                        @if ($d->file_surat)
-                            <a href="{{ asset('storage/'.$d->file_surat) }}" target="_blank">Download</a>
-                        @else
-                            -
-                        @endif
-                    </td>
+                    <th>Tahun Akademik</th>
+                    <th>Jumlah</th>
+                    <th>No HP</th>
+                    <th>Deadline</th>
+                    <th>Status</th>
+                    <th>File</th>
                 </tr>
-            @empty
-                <tr>
-                    <td colspan="6" class="text-center">Belum ada pengajuan dispensasi</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+            </thead>
 
-</div>
+            <tbody>
+                @forelse ($data as $d)
+                    <tr>
+                        <td>{{ $d->tahun_akademik }}</td>
+                        <td>{{ $d->jumlah_pengajuan }}</td>
+                        <td>{{ $d->no_hp }}</td>
+                        <td>{{ $d->tanggal_deadline }}</td>
+                        <td>
+                            @if ($d->status == 'menunggu')
+                                <span class="badge bg-warning">Menunggu</span>
+                            @elseif ($d->status == 'disetujui')
+                                <span class="badge bg-success">Disetujui</span>
+                            @else
+                                <span class="badge bg-danger">Ditolak</span>
+                            @endif
+                        </td>
+                        <td>
+                            @if ($d->file_surat)
+                                <a href="{{ asset('storage/' . $d->file_surat) }}" target="_blank">Download</a>
+                            @else
+                                -
+                            @endif
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6" class="text-center">Belum ada pengajuan dispensasi</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
 
+    </div>
 @endsection
+
+@push('styles')
+    <link href="{{ url('css/dashboard.css') }}" rel="stylesheet" />
+@endpush
